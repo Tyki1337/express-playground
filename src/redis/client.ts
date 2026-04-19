58f71bd5c9b64a2b34c7e9a6f0db0c135b095406
Tyki <tyki1713@mail.ru>
2026-04-19 13:24:54 +0300

    Implemented redis

---
+++ src/redis/client.ts
@@ -0,0 +1,17 @@
+import "dotenv/config"
+import { createClient } from 'redis';
+
+const client = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASSWORD /*|| "PkGDr4V5NDawzB03affnZJMnTLUmb90K"*/,
+    socket: {
+        host: 'redis-15496.c293.eu-central-1-1.ec2.cloud.redislabs.com',
+        port: 15496
+    }
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+console.log(`Пароль из ENV: [${process.env.REDIS_PASSWORD}]`);
+await client.connect()
+export default client
+
